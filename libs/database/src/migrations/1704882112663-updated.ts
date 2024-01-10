6eb361d0f37eec24be7d8d2df341ac53ddedcbb9
ANISH-18 <aniruddhabhavake018@gmail.com>
2024-01-10 15:55:08 +0530

    Changes Done

--- libs/database/src/migrations/1704882112663-updated.ts
+++ libs/database/src/migrations/1704882112663-updated.ts
@@ -1,13 +1,15 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateUser1704822041996 implements MigrationInterface {
-    name = 'CreateUser1704822041996'
+export class Updated1704882112663 implements MigrationInterface {
+    name = 'Updated1704882112663'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying, "phone_number" character varying, "password" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "order" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "user_id" character varying, "order_name" character varying, "sub_total" character varying, "phone_number" character varying, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_1031171c13130102495201e3e20" PRIMARY KEY ("id"))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "order"`);
         await queryRunner.query(`DROP TABLE "user"`);
     }
 
--- libs/database/src/migrations/index.ts
+++ libs/database/src/migrations/index.ts
@@ -1,13 +1,5 @@
-import { CreateUser1704822041996 } from './1704822041996-createUser';
-import { Orders1704829301441 } from './1704829301441-orders';
-import { Updated1704864457843 } from './1704864457843-updated';
+import { Updated1704882112663 } from './1704882112663-updated';
 
-export const migrations = [
-  CreateUser1704822041996,
-  Orders1704829301441,
-  Updated1704864457843,
-];
+export const migrations = [Updated1704882112663];
 
-export * from './1704822041996-createUser';
-export * from './1704829301441-orders';
-export * from './1704864457843-updated';
+export * from './1704882112663-updated';
